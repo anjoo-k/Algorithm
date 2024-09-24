8368238d8f79fe82520c43205351ccdc60f9a660
anjoo-k <160109778+anjoo-k@users.noreply.github.com>
2024-09-25 00:36:28 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 676 ms, Memory: 56140 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -1,37 +1,48 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.BufferedWriter;
 import java.io.IOException;
- 
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
 public class Main {
+
+	static int[][] arr;
+	
 	public static void main(String[] args) throws IOException {
- 
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		int N = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
+		arr = new int [n][2];
 		
-		int[][] arr = new int[N][2];
-		
-		StringTokenizer st;
-		for(int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 			arr[i][0] = Integer.parseInt(st.nextToken());
 			arr[i][1] = Integer.parseInt(st.nextToken());
 		}
 		
-		Arrays.sort(arr, (e1, e2) -> {
-			if(e1[0] == e2[0]) {
-				return e1[1] - e2[1];
-			} else {
-				return e1[0] - e2[0];
+		
+		Arrays.sort(arr, new Comparator<int[]>() {
+
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				return o1[0] != o2[0]? o1[0] - o2[0] : o1[1] - o2[1];
 			}
+			
 		});
 		
-		StringBuilder sb = new StringBuilder();
-		for(int i = 0 ; i< N ; i++) {
-			sb.append(arr[i][0] + " " + arr[i][1]).append('\n');
+		for(int i = 0; i < n; i++) {
+			sb.append(arr[i][0] + " " + arr[i][1]).append("\n");
 		}
-		System.out.println(sb);
+		
+		bw.write(sb.toString());
+		bw.flush();
+		br.close();
+		bw.close();
 	}
 }
